6b899ddcb2f4772c4e4ea865540ed953a95d878b
Armando Tavera <taveramex@gmail.com>
2025-07-16 17:08:04 -0600

    little changes on style to add new behaviors, new buttons in counterApp

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,22 +1,32 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 
 
-function CounterApp({value}){
-    return(
+
+function CounterApp({ value }) {
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => setCounter(counter+1)
+    const handleRemove = () => setCounter(counter-1)
+    const handleReset = () => setCounter(value)
+    return (
         <>
-        <h1>The Value is:</h1>
-        <p>{value}</p>
+            <h1>The Value is:</h1>
+            <p> {counter} </p>
+            <button onClick={handleAdd} className="matrix-button"> +1 </button>
+            <button onClick={handleRemove} className="matrix-button"> -1 </button>
+            <button onClick={handleReset} className="matrix-button"> reset </button>
         </>
     );
 }
 
 CounterApp.propTypes = {
-    value : PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 }
 
 CounterApp.defaultProps = {
-    value : 0,
+    value: 0,
 }
 
 export { CounterApp }
